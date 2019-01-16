244f29a579c05ce51304c9beaed424f250cd4b1e
thinhdv <thinhdv90@gmail.com>
2019-01-16 17:37:53 +0700

    add ADM-tools.groovy

---
+++ ADM-tools.groovy
@@ -0,0 +1,45 @@
+@Library('admin-groovy-libs@master') _
+
+//Variables
+def pkg = PKG.trim()
+def srv = SRV
+String pkgName = ""
+String pkgVer = ""
+pkgInfo = pkgInfo.getPkgInfo(pkg)
+pkgName = pkgInfo[0]
+pkgVer = pkgInfo[1]
+
+pkgPermission.checkPkgPermission(pkgName, srv)
+
+def deploy_pkg(srv, pkg) {
+    node("master") {
+        sleep(5)
+        sh(script: "python /sysadmin/jenkins-script/jdeploy.py -m ${srv} -p ${pkg}")
+    }
+}
+
+def check_service(srv, pkgName) {
+    node("monitoring") {
+        sleep(5)
+        sh(script: "python /sysadmin/jenkins-script/application_check.py ${pkgName} ${srv}")
+        sh(script: "python /sysadmin/jenkins-script/service_check.py ${pkgName} ${srv}")
+    }
+}
+
+currentBuild.displayName = "#" + currentBuild.number + " - " + pkgName + "=" + pkgVer + " - " + SRV
+
+try {
+    stage("Downtime") {
+        println "Downtime ..."
+    }
+    stage("Deloying") {
+        deploy_pkg(srv, 'atop=2.3.0-1')
+    }
+    stage("Rechecking") {
+        check_service(srv, pkgName)
+    }
+}
+catch(Exception exp) {
+    currentBuild.result = "FAILURE"
+}
+
